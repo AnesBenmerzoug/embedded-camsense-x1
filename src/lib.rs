877206217b3e45912b4523a3a74474f137cacfa8
Anes Benmerzoug <anes.benmerzoug@gmail.com>
2026-06-19 12:31:43 +0200

    Compute speed only instead of frequency, remove unused code, fix off by one error when computing angle step, check distance flag bit

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ mod camsense_x1;
 mod constants;
 mod types;
 
-pub use camsense_x1::{Config, Camsense};
+pub use camsense_x1::{Camsense, Config};
 pub use types::{Error, Measurement, Scan};
--- src/types.rs
+++ src/types.rs
@@ -22,6 +22,7 @@ pub enum Error<E> {
 pub struct RawDistance {
     value: u16,
     quality: u8,
+    flag: bool,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -80,11 +81,23 @@ impl TryFrom<[u8; 36]> for RawMeasurement {
         let mut distances = [RawDistance {
             value: 0,
             quality: 0,
+            flag: false,
         }; 8];
         for i in 0..8 {
+            let distance_bytes = [data[8 + i * 3], data[9 + i * 3], data[10 + i * 3]];
+            // Mask left-most bit of second byte
+            let value_bytes = [distance_bytes[0], distance_bytes[1] & 0x3F];
+            let value = u16::from_le_bytes(value_bytes);
+            let quality = distance_bytes[2];
+            // Flag this value as invalid if the flag bit is set
+            // or the distance is 0
+            // or the quality is 0
+            let flag = (distance_bytes[1] >> 7) & 0x01 == 1 || value == 0 || quality == 0;
+
             let distance = RawDistance {
-                value: u16::from_le_bytes([data[8 + i * 3], data[9 + i * 3]]),
-                quality: data[10 + i * 3],
+                value,
+                quality,
+                flag,
             };
             distances[i] = distance;
         }
@@ -110,7 +123,7 @@ pub struct Point {
 #[derive(Clone, Copy, Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct Measurement {
-    pub frequency: f32,
+    pub speed: f32,
     pub start_angle: f32,
     pub end_angle: f32,
     pub points: [Option<Point>; 8],
@@ -118,31 +131,23 @@ pub struct Measurement {
 
 impl From<(RawMeasurement, f32)> for Measurement {
     fn from((raw, angle_offset): (RawMeasurement, f32)) -> Self {
-        let frequency = raw.speed as f32 / 60.0;
+        let speed = raw.speed as f32 / 64.0;
         let start_angle = raw.start_angle as f32 / 64.0 - 640.0;
         let end_angle = raw.end_angle as f32 / 64.0 - 640.0;
         let step = if end_angle > start_angle {
-            (end_angle - start_angle) / 8.0
+            (end_angle - start_angle) / 7.0
         } else {
-            (end_angle - (start_angle - 360.0)) / 8.0
+            (end_angle - (start_angle - 360.0)) / 7.0
         };
 
         let mut points = [None; 8];
-        let mut min_index = None;
-        //println!("start: {}, end: {}, step: {}", start_angle, end_angle, step);
         for (i, raw_distance) in raw.distances.iter().enumerate() {
-            if raw_distance.quality == 0 {
+            if raw_distance.flag {
                 continue;
             }
+
             let angle = (start_angle + step * i as f32 + angle_offset) % 360.0;
             let index = (angle * INDEX_MULTIPLIER).round() as usize % 400;
-            if min_index.is_none() {
-                min_index = Some(index);
-            } else {
-                if index < min_index.unwrap() {
-                    min_index = Some(index);
-                }
-            }
             points[i] = Some(Point {
                 distance: raw_distance.value,
                 angle,
@@ -150,7 +155,7 @@ impl From<(RawMeasurement, f32)> for Measurement {
             });
         }
         Self {
-            frequency,
+            speed,
             start_angle,
             end_angle,
             points,
